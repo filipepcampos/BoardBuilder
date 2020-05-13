b310e8a17820fdd8888413f9982d46b924a52583
Filipe Campos <filipepcampos24@gmail.com>
2020-05-13 18:06:12 +0100

    Restructure code

---
+++ cmake-build-debug/CMakeFiles/BoardBuilder.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/user/Desktop/boardbuilder/Board.cpp" "/mnt/c/Users/user/Desktop/boardbuilder/cmake-build-debug/CMakeFiles/BoardBuilder.dir/Board.cpp.o"
+  "/mnt/c/Users/user/Desktop/boardbuilder/IO.cpp" "/mnt/c/Users/user/Desktop/boardbuilder/cmake-build-debug/CMakeFiles/BoardBuilder.dir/IO.cpp.o"
+  "/mnt/c/Users/user/Desktop/boardbuilder/Word.cpp" "/mnt/c/Users/user/Desktop/boardbuilder/cmake-build-debug/CMakeFiles/BoardBuilder.dir/Word.cpp.o"
+  "/mnt/c/Users/user/Desktop/boardbuilder/main.cpp" "/mnt/c/Users/user/Desktop/boardbuilder/cmake-build-debug/CMakeFiles/BoardBuilder.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/BoardBuilder.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/BoardBuilder.dir/main.cpp.o"
+  "CMakeFiles/BoardBuilder.dir/Board.cpp.o"
+  "CMakeFiles/BoardBuilder.dir/IO.cpp.o"
+  "CMakeFiles/BoardBuilder.dir/Word.cpp.o"
+  "BoardBuilder.pdb"
+  "BoardBuilder"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BoardBuilder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
